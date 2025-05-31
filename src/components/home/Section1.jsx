3eb37408c2a8e84d71211be2889e37b39196cc9e
muk1mov13 <ilhomjonmuk1mov0770@gmail.com>
2025-05-31 14:41:41 +0500

    updated section1

--- src/components/home/Section1.jsx
+++ src/components/home/Section1.jsx
@@ -260,8 +260,8 @@ function Section1(props) {
 
                 <div className="relative w-full min-h-[100vh] overflow-hidden">
                     <div className="absolute inset-0">
-                        <div className="relative flex h-full flex-col items-start justify-center w-full z-20 text-white px-6 md:px-12 py-6 md:py-0">
-                            <div className="max-w-3xl text-left bg-black bg-opacity-10 p-6 md:p-8 rounded-lg">
+                        <div className="relative flex h-full flex-col items-start justify-center w-full z-20 text-[#012152] px-6 md:px-12 py-6 md:py-0">
+                            <div className="max-w-3xl text-left bg-white opacity-80 p-6 md:p-8 rounded-lg">
                                 {/* Welcome Text with Backspace Animation */}
                                 <div className="flex items-center">
                                     <motion.span
