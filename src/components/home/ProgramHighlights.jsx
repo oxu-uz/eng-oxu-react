1abbacdfb38891760a9d515b1900df3e32123bdd
muk1mov13 <ilhomjonmuk1mov0770@gmail.com>
2025-07-15 16:57:01 +0500

    changed ProgramHighlights items hover color

--- src/components/home/ProgramHighlights.jsx
+++ src/components/home/ProgramHighlights.jsx
@@ -63,7 +63,7 @@ const ProgramHighlights = () => {
                                     initial={false}
                                     animate={{
                                         background: hoveredIndex === index
-                                            ? 'linear-gradient(to top, rgba(0,0,0,0.8) 0%, rgba(0,0,0,0) 100%)'
+                                            ? 'linear-gradient(to top, rgba(0,51,102,0.8) 0%, rgba(0,91,187,0) 100%)'
                                             : 'linear-gradient(to top, rgba(0,0,0,0.6) 0%, rgba(0,0,0,0) 100%)'
                                     }}
                                     transition={{ duration: 0.3 }}
@@ -116,14 +116,6 @@ const ProgramHighlights = () => {
                             >
                                 <div className="absolute inset-0 opacity-10 bg-[url('/header1.png')] bg-repeat " />
                                 FIND YOUR PROGRAM
-                                {/*<svg*/}
-                                {/*    xmlns="http://www.w3.org/2000/svg"*/}
-                                {/*    className="h-5 w-5 ml-2"*/}
-                                {/*    viewBox="0 0 20 20"*/}
-                                {/*    fill="currentColor"*/}
-                                {/*>*/}
-                                {/*    <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />*/}
-                                {/*</svg>*/}
                             </button>
                         </div>
                     </div>
